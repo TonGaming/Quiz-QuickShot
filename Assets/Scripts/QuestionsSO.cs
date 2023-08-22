b818b354d984755ef341e2a556d9834b47b6ec2c
LadyKillerr <dungvuduc0509@gmail.com>
2023-08-22 18:27:29 +0700

    update: uploading more key features

---
+++ Assets/Scripts/QuestionsSO.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")] 
+public class QuestionsSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+}
