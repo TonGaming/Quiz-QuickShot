f54558f6b33c32d74451050e87a8d0d54df6b93e
LadyKillerr <dungvuduc0509@gmail.com>
2023-08-23 11:20:33 +0700

    uploading

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -10,6 +10,9 @@ public class Quiz : MonoBehaviour
 
     private void Start()
     {
+        /* truy cập vào text của questionText, gán cho nó giá trị của 
+        * question, giá trị của question thì truy cập tới kết quả
+        * question của hảm GetQuestion() ở QuestionsSO */
         questionText.text = question.GetQuestion();
     }
 }
