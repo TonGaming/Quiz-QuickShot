555454ebc01ebb5c0ba7c6f7a3c2549b11866c84
LadyKillerr <dungvuduc0509@gmail.com>
2023-09-01 17:56:02 +0700

    feat: added slider features

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulations!\nYou got a score of " +
+             scoreKeeper.CalculateScore() + "%";
+    }
+
+    
+}
